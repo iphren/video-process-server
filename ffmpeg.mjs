c63b03919d4411e550cb246ac22926422584dbf3
iphren <contact@iphren.com>
2022-02-23 13:17:16 +0000

    add basic ffmpeg tracking

---
+++ ffmpeg.mjs
@@ -0,0 +1,44 @@
+import { spawn } from 'child_process';
+
+const processes = {};
+
+function run(command) {
+    const process = spawn(command, { shell: true });
+    const startTime = Date.now();
+    const pid = `${startTime}${process.pid}`;
+
+    processes[pid]  = {
+        startedAt: startTime,
+        finishedAt: -1,
+        exitCode: -1,
+        done: false,
+        duration: 0,
+        currentTime: 0
+    };
+
+    // ffmpeg output to stderr
+    process.stderr.on('data', (data) => {
+        const text = `${data}`;
+        if (!processes[pid].duration) {
+            const m = text.match(/Duration: ([0-9]+):([0-9]+):([0-9]+.[0-9]+)/);
+            if (m) {
+                processes[pid].duration = +m[1] * 3600 + +m[2] * 60 + +m[3];
+            }
+        } else {
+            const m = text.match(/ time=([0-9]+):([0-9]+):([0-9]+.[0-9]+) /);
+            if (m) {
+                processes[pid].currentTime = +m[1] * 3600 + +m[2] * 60 + +m[3];
+            }
+        }
+    });
+
+    process.on('close', (code) => {
+        processes[pid].finishedAt = Date.now();
+        processes[pid].exitCode = code;
+        processes[pid].done = true;
+    });
+
+    return pid;
+}
+
+export { run, processes }
--- router.mjs
+++ router.mjs
@@ -1,9 +1,23 @@
 import express from "express";
+import { run, processes } from "./ffmpeg.mjs";
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send(req.url);
+    res.json(processes);
+});
+
+router.post('/', (req, res) => {
+    const pid = run(req.body.command);
+    res.json({ pid: pid });
+});
+
+router.get('/:pid', (req, res) => {
+    if (req.params.pid in processes) {
+        res.json(processes[req.params.pid]);
+    } else {
+        res.status(404).end();
+    }
 });
 
 export { router }
